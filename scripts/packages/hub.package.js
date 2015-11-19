5ef935c2a4e1746d80919203057f3afef09459d2
alexbowen <alexbowen123@googlemail.com>
2015-11-19 15:08:31 +0000

    added missing hub files

---
+++ scripts/packages/hub.package.js
@@ -0,0 +1,11 @@
+define([
+    'app',
+    'modules/common',
+    'pages/hub/hub.module'
+], function(app) {
+    'use strict';
+
+    app.requires.push('comMarksandspencerApp.pages.hub');
+    return app;
+});
+

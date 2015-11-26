943bd49b6a9a81f5c9106bcfc9c4d6aa694865f5
alexbowen <alexbowen123@googlemail.com>
2015-11-26 17:35:28 +0000

    removed unneded files

--- test/cucumber/features/step_definitions/patterns/base/typography.js
+++
@@ -1,6 +0,0 @@
-module.exports = function() {
-
-//    this.Then(/^I should see a ([^']+) h1 tag$/, function (arg1, callback) {
-//        this.expect(this.page.headerLevelOne.getCssValue('font-size')).to.eventually.equal(arg1).and.notify(callback);
-//    });
-};
